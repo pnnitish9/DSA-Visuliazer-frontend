f95efdcee082909c7745de31271fce4718ec7aab
Nitish Kr <pnnitish88725@gmail.com>
2025-10-31 02:02:15 +0530

    css upadate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "./../context/AuthContext";
-import { User } from "lucide-react"; // 👈 Import person icon
+import { User } from "lucide-react"; //Import person icon
 
 function Navbar() {
   const { isAuthenticated, user, logout } = useAuth();
